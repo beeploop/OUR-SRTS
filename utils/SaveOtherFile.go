ca18ff00077e4e6373a648772dbc42fa570bda6e
John Loyd Mulit <jlmulit68@gmail.com>
2023-11-25 19:24:49 +0800

    fix: basefolder not found in SaveOtherFile

--- utils/SaveOtherFile.go
+++ utils/SaveOtherFile.go
@@ -6,13 +6,14 @@ import (
 	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/registrar/config"
 )
 
 func SaveOtherFile(filename, lastname, controlNumber, key string, file *multipart.FileHeader, c *gin.Context) (string, string, error) {
 	transformedFilename := strings.ReplaceAll(filename, " ", "-")
 	ext := filepath.Ext(file.Filename)
 	newFilename := transformedFilename + ext
-	path := BASE_FOLDER + newFilename
+	path := config.BASE_FOLDER + newFilename
 
 	err := c.SaveUploadedFile(file, path)
 	if err != nil {
