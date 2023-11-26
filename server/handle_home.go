977f6bb65c458d037d40773df164120f95ba06ac
John Loyd Mulit <jlmulit68@gmail.com>
2023-11-26 23:51:23 +0800

    chore: clean up comments in handle_home function

--- server/handle_home.go
+++ server/handle_home.go
@@ -4,15 +4,9 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	// "github.com/registrar/utils"
 )
 
 func HandleHome(c *gin.Context) {
 	c.Request.Method = "GET"
 	c.Redirect(http.StatusSeeOther, "/auth/login")
-
-	// html := utils.HtmlParser("index.html", "components/head.html", "components/header.html")
-	// Router.SetHTMLTemplate(html)
-	//
-	// c.HTML(http.StatusOK, "index.html", nil)
 }
